fb5eafbb744c1af55283900dca5a4ecbd1635785
xienengxian <xnx@xingzheyi.com>
2018-08-08 17:34:16 +0800

    新增角色实体 新增角色实体类，进行权限控制

---
+++ src/main/java/com/entity/Role.java
@@ -0,0 +1,31 @@
+package com.entity;
+
+public class Role {
+    private Integer roleId;
+    private String roleName;
+    private String permission;
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getPermission() {
+        return permission;
+    }
+
+    public void setPermission(String permission) {
+        this.permission = permission;
+    }
+}
